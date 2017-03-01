688cab37ce726f13cefb5c9acffe1b14bfe34ebd
weibiao <ybilland@163.com>
2017-03-01 10:12:02 +0800

    .

--- app/src/main/java/com/lmj/com/mynestscroll/view/MyNestedScrollChildL.java
+++ app/src/main/java/com/lmj/com/mynestscroll/view/MyNestedScrollChildL.java
@@ -7,66 +7,60 @@ import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 import android.widget.LinearLayout;
 
 public class MyNestedScrollChildL extends LinearLayout implements NestedScrollingChild {
-    private String Tag = "MyNestedScrollChild";
     private NestedScrollingChildHelper mScrollingChildHelper;
     private final int[] mScrollOffset = new int[2];
     private final int[] mScrollConsumed = new int[2];
-    private final int[] mNestedOffsets = new int[2];
     private int mLastTouchX;
-    private int mScrollPointerId;
     private int mLastTouchY;
-    private int mTouchSlop;
-    private boolean mIsBeingDragged;
     private int showHeight;
 
     public MyNestedScrollChildL(Context context) {
         super(context);
-        init(context);
+        init();
     }
 
     public MyNestedScrollChildL(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init();
     }
 
 
-    public void init(Context context) {
-        final ViewConfiguration configuration = ViewConfiguration.get(context);
-        mTouchSlop = configuration.getScaledTouchSlop();
+    public void init() {
+        mScrollingChildHelper = new NestedScrollingChildHelper(this);
+        mScrollingChildHelper.setNestedScrollingEnabled(true);
     }
 
     @Override
     public void setNestedScrollingEnabled(boolean enabled) {
         Log.e("Bill", "NestedScrollingChild setNestedScrollingEnabled:" + enabled);
-        getScrollingChildHelper().setNestedScrollingEnabled(enabled);
+        mScrollingChildHelper.setNestedScrollingEnabled(enabled);
     }
 
     @Override
     public boolean isNestedScrollingEnabled() {
         Log.e("Bill", "NestedScrollingChild isNestedScrollingEnabled");
-        return getScrollingChildHelper().isNestedScrollingEnabled();
+        return mScrollingChildHelper.isNestedScrollingEnabled();
     }
 
     @Override
     public boolean startNestedScroll(int axes) {
         Log.e("Bill", "NestedScrollingChild startNestedScroll :" + axes);
-        return getScrollingChildHelper().startNestedScroll(axes);
+        return mScrollingChildHelper.startNestedScroll(axes);
     }
 
     @Override
     public void stopNestedScroll() {
         Log.e("Bill", "NestedScrollingChild stopNestedScroll");
-        getScrollingChildHelper().stopNestedScroll();
+        mScrollingChildHelper.stopNestedScroll();
     }
 
     @Override
     public boolean hasNestedScrollingParent() {
         Log.e("Bill", "NestedScrollingChild hasNestedScrollingParent");
-        return getScrollingChildHelper().hasNestedScrollingParent();
+        return mScrollingChildHelper.hasNestedScrollingParent();
     }
 
     @Override
@@ -75,35 +69,32 @@ public class MyNestedScrollChildL extends LinearLayout implements NestedScrollin
         Log.e("Bill", "NestedScrollingChild dispatchNestedScroll:dxConsumed:" + dxConsumed + "," +
                 "dyConsumed:" + dyConsumed + ",dxUnconsumed:" + dxUnconsumed + ",dyUnconsumed:" +
                 dyUnconsumed + ",offsetInWindow:" + offsetInWindow);
-        return getScrollingChildHelper().dispatchNestedScroll(dxConsumed, dyConsumed,
+        return mScrollingChildHelper.dispatchNestedScroll(dxConsumed, dyConsumed,
                 dxUnconsumed, dyUnconsumed, offsetInWindow);
     }
 
     @Override
     public boolean dispatchNestedPreScroll(int dx, int dy, int[] consumed, int[] offsetInWindow) {
         Log.e("Bill", "NestedScrollingChild dispatchNestedPreScroll:dx" + dx + ",dy:" + dy + ",consumed:" + consumed + ",offsetInWindow:" + offsetInWindow);
-        return getScrollingChildHelper().dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow);
+        return mScrollingChildHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow);
     }
 
     @Override
     public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
         Log.e("Bill", "NestedScrollingChild dispatchNestedFling:velocityX:" + velocityX + ",velocityY:" + velocityY + ",consumed:" + consumed);
-        return getScrollingChildHelper().dispatchNestedFling(velocityX, velocityY, consumed);
+        return mScrollingChildHelper.dispatchNestedFling(velocityX, velocityY, consumed);
     }
 
     @Override
     public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
         Log.e("Bill", "NestedScrollingChild dispatchNestedPreFling:velocityX:" + velocityX + ",velocityY:" + velocityY);
-        return getScrollingChildHelper().dispatchNestedPreFling(velocityX, velocityY);
+        return mScrollingChildHelper.dispatchNestedPreFling(velocityX, velocityY);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
         switch (e.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-
-            {
-
+            case MotionEvent.ACTION_DOWN: {
                 mLastTouchY = (int) (e.getRawY() + 0.5f);
                 int nestedScrollAxis = ViewCompat.SCROLL_AXIS_NONE;
 
@@ -112,51 +103,32 @@ public class MyNestedScrollChildL extends LinearLayout implements NestedScrollin
             }
             break;
             case MotionEvent.ACTION_MOVE: {
-                Log.i("aaa", "Child--getRawY:" + e.getRawY());
-
                 int x = (int) (e.getX() + 0.5f);
                 int y = (int) (e.getRawY() + 0.5f);
                 int dx = mLastTouchX - x;
                 int dy = mLastTouchY - y;
-                Log.i(Tag, "child:dy:" + dy + ",mLastTouchY:" + mLastTouchY + ",y;" + y);
-                Log.i(Tag, "xxx");
                 mLastTouchY = y;
                 mLastTouchX = x;
                 if (dispatchNestedPreScroll(dx, dy, mScrollConsumed, mScrollOffset)) {
-
                     dy -= mScrollConsumed[1];
                     if (dy == 0) {
                         return true;
                     }
-
                 } else {
                     scrollBy(0, dy);
-
                 }
             }
-
 
         }
 
         return true;
     }
 
-    private NestedScrollingChildHelper getScrollingChildHelper() {
-        if (mScrollingChildHelper == null) {
-            mScrollingChildHelper = new NestedScrollingChildHelper(this);
-            mScrollingChildHelper.setNestedScrollingEnabled(true);
-        }
-        return mScrollingChildHelper;
-    }
-
     @Override
     public void scrollTo(int x, int y) {
-
-        int sy = getScrollY();
-        int mh = getMeasuredHeight();
-        int MaxY = getMeasuredHeight() - showHeight;
-        if (y > MaxY) {
-            y = MaxY;
+        int maxY = getMeasuredHeight() - showHeight;
+        if (y > maxY) {
+            y = maxY;
         }
         if (y < 0) {
             y = 0;
--- app/src/main/java/com/lmj/com/mynestscroll/view/MyNestedScrollParent.java
+++ app/src/main/java/com/lmj/com/mynestscroll/view/MyNestedScrollParent.java
@@ -8,23 +8,19 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 import android.widget.Scroller;
-import android.widget.TextView;
 
 public class MyNestedScrollParent extends RelativeLayout implements NestedScrollingParent {
-    private String Tag = "MyNestedScrollParent";
-//    private TextView tv;
-    private int titleRowNum;
-    private MyNestedScrollChildL nsc0;
     private MyNestedScrollChildL nsc1;
+    private MyNestedScrollChildL nsc0;
     private NestedScrollingParentHelper mParentHelper;
 //    private int tvHeight;
+//    private TextView tv;
 
     private int length = (int) TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, 100, getContext().getResources().getDisplayMetrics()
-    ); // 半径
+    ); // 滑动的距离
 
     private Scroller scroller;
 
@@ -66,55 +62,55 @@ public class MyNestedScrollParent extends RelativeLayout implements NestedScroll
         });*/
 
 
+    }
 
+    private void init() {
+        mParentHelper = new NestedScrollingParentHelper(this);
+        scroller = new Scroller(getContext());
     }
 
     @Override
     public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        // 嵌套滑动的第一个方法
         Log.e("Bill", "NestedScrollingParent onStartNestedScroll");
         return true;
-    }
-    private void init() {
-        mParentHelper = new NestedScrollingParentHelper(this);
-        scroller = new Scroller(getContext());
-
     }
 
     @Override
     public void onNestedScrollAccepted(View child, View target, int nestedScrollAxes) {
-        Log.e("Bill", "NestedScrollingParent onNestedScrollAccepted"+"child:"+child+",target:"+target+",nestedScrollAxes:"+nestedScrollAxes);
+        Log.e("Bill", "NestedScrollingParent onNestedScrollAccepted" + "child:" + child + ",target:" + target + ",nestedScrollAxes:" + nestedScrollAxes);
         mParentHelper.onNestedScrollAccepted(child, target, nestedScrollAxes);
     }
 
     @Override
     public void onStopNestedScroll(View target) {
-        Log.e("Bill", "NestedScrollingParent onStopNestedScroll--target:"+target);
+        Log.e("Bill", "NestedScrollingParent onStopNestedScroll--target:" + target);
         mParentHelper.onStopNestedScroll(target);
     }
 
     @Override
     public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        Log.e("Bill", "NestedScrollingParent onNestedScroll--"+"target:"+target+",dxConsumed"+dxConsumed+",dyConsumed:"+dyConsumed
-        +",dxUnconsumed:"+dxUnconsumed+",dyUnconsumed:"+dyUnconsumed);
+        Log.e("Bill", "NestedScrollingParent onNestedScroll--" + "target:" + target + ",dxConsumed" + dxConsumed + ",dyConsumed:" + dyConsumed
+                + ",dxUnconsumed:" + dxUnconsumed + ",dyUnconsumed:" + dyUnconsumed);
     }
 
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-       if(dy > 0 && getScrollY() < length){ // 上拉
-           consumed[1]=length;
+        if (dy > 0 && getScrollY() < length) { // 上拉
+            consumed[1] = length;
             scroller.startScroll(0, getScrollY(), 0, length);
             invalidate();
-        }else if(dy < 0 && getScrollY()>0){ // 下滑
-           if(target == nsc0 || (target == nsc1 && nsc1.getScrollY() == 0)){
-               consumed[1]=length;
-               scroller.startScroll(0, getScrollY(), 0, -length);
-               invalidate();
-           }
+        } else if (dy < 0 && getScrollY() > 0) { // 下滑
+            if (target == nsc0 || (target == nsc1 && nsc1.getScrollY() == 0)) {
+                consumed[1] = length;
+                scroller.startScroll(0, getScrollY(), 0, -length);
+                invalidate();
+            }
         }
         int num = -1;
-        if(target == nsc0){
+        if (target == nsc0) {
             num = 1;
-        }else if(target == nsc1){
+        } else if (target == nsc1) {
             num = 2;
         }
         Log.e("Bill3", "nsc0:::" + nsc0.getScrollY() + "nsc1:" + nsc1.getScrollY() + "|view:" + num);
@@ -128,18 +124,18 @@ public class MyNestedScrollParent extends RelativeLayout implements NestedScroll
 //            invalidate();
             Log.i("onNestedPreScroll","Parent滑动："+dy);
         }*/
-        Log.e("Bill2", "NestedScrollingParent onNestedPreScroll--getScrollY():"+getScrollY()+",dx:"+dx+",dy:"+dy+",consumed:"+nsc1.getScrollY());
+        Log.e("Bill2", "NestedScrollingParent onNestedPreScroll--getScrollY():" + getScrollY() + ",dx:" + dx + ",dy:" + dy + ",consumed:" + nsc1.getScrollY());
     }
 
     @Override
     public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
-        Log.e("Bill", "NestedScrollingParent onNestedFling--target:"+target);
+        Log.e("Bill", "NestedScrollingParent onNestedFling--target:" + target);
         return false;
     }
 
     @Override
     public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
-        Log.e("Bill", "NestedScrollingParent onNestedPreFling--target:"+target);
+        Log.e("Bill", "NestedScrollingParent onNestedPreFling--target:" + target);
         return false;
     }
 
@@ -151,22 +147,22 @@ public class MyNestedScrollParent extends RelativeLayout implements NestedScroll
 
     @Override
     public void scrollTo(int x, int y) {
-        if(y<0){
-            y=0;
+        if (y < 0) {
+            y = 0;
         }
-        if(y>length){
-            y=length;
+        if (y > length) {
+            y = length;
         }
 
         super.scrollTo(x, y);
     }
 
     /**
-    下拉的时候是否要向下滑动显示图片
+     * 下拉的时候是否要向下滑动显示图片
      */
-    public boolean showImg(int dy){
-        if(dy<0){
-            if(getScrollY()>0&&nsc1.getScrollY()==0){//如果parent外框，还可以往上滑动
+    public boolean showImg(int dy) {
+        if (dy < 0) {
+            if (getScrollY() > 0 && nsc1.getScrollY() == 0) {//如果parent外框，还可以往上滑动
                 return true;
             }
         }
@@ -177,11 +173,12 @@ public class MyNestedScrollParent extends RelativeLayout implements NestedScroll
 
     /**
      * 上拉的时候，是否要向上滑动，隐藏图片
+     *
      * @return
      */
-    public boolean hideImg(int dy){
-        if(dy>0){
-            if(getScrollY()<length){//如果parent外框，还可以往下滑动
+    public boolean hideImg(int dy) {
+        if (dy > 0) {
+            if (getScrollY() < length) {//如果parent外框，还可以往下滑动
                 return true;
             }
         }
@@ -190,13 +187,11 @@ public class MyNestedScrollParent extends RelativeLayout implements NestedScroll
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         return super.onTouchEvent(event);
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        Log.i("aaa","getY():getRawY:"+event.getRawY());
         return super.dispatchTouchEvent(event);
     }
 }
