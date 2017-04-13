f1809b81360a429a8298df967d87533b431859b8
weibiao <ybilland@163.com>
2017-04-13 17:32:36 +0800

    优化

---
+++ app/src/main/java/com/lmj/com/mynestscroll/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.lmj.com.mynestscroll;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lmj.com.mynestscroll.view2.ImageBottomLayout;
+
+/**
+ * Created by Bill on 2017/2/28.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ImageBottomLayout mBottomLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mBottomLayout = (ImageBottomLayout) findViewById(R.id.bottom_layout);
+        mBottomLayout.setTtitle("这是一个Title");
+        mBottomLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mBottomLayout.setContent(getString(R.string.large_text));
+            }
+        }, 20);
+
+    }
+
+}
